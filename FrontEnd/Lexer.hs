7d4b16d46910725c3897ffb486c3f9c4161fbf45
suhorng <suhorng@gmail.com>
2013-11-13 00:07:34 +0800

    add token defns.

--- FrontEnd/Lexer.hs
+++ FrontEnd/Lexer.hs
@@ -1,8 +1,25 @@
 module FrontEnd.Lexer (
   Token(..),
+  Literal(..),
   lexer
 ) where
 
-data Token = Hello
+data Token = LiteralToken Literal
+           | ID String
+           | SymArithmetic Char
+           | SymRelational Char
+           | SymLogic Char
+           | SymAssign Char {- though this character can only be '=' -}
+           | SymSeparator Char
+
+data Literal = IntLiteral Integer
+             | FloatLiteral Double
+             | StringLiteral String
+
+symbols = [(["+","-","*","/"], SymArithmetic),
+           (["<=", ">=", "!=", "==", "<", ">"], SymRelational), -- note that the 
+           (["||", "&&", "!"], SymLogic),
+           (["="], SymAssign),
+           (["{", "}", "[", "]", "(", ")", ";", ",", "."], SymSeparator)]
 
 lexer = undefined
