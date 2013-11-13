414decbd437a6b8e65a3ac34876adf03b6306276
suhorng <suhorng@gmail.com>
2013-11-13 18:51:06 +0800

    Parser.AST loves you part I

--- Language/BLang/FrontEnd/ParsedAST.hs
+++ Language/BLang/FrontEnd/ParsedAST.hs
@@ -1,5 +1,31 @@
 module Language.BLang.FrontEnd.ParsedAST (
-  AST(..)
+  AST(..),
+  Decl(..),
+  Type(..)
 ) where
 
-data AST
+data AST = Program [AST]
+         | VarDeclList [Decl]
+         | FunctDecl Type String [(String, Type)]
+         | Block
+         | Stmt
+         | BinaryOp
+         | UnaryOp
+         | ConstVal
+         | AssignExprList
+         | RelOpExprList
+         deriving (Show)
+
+data Decl = TypeDecl Type [String]
+          | VarDecl Type [String]
+          deriving (Show)
+
+data Type = TInt
+          | TFloat
+          | TVoid
+          | TChar
+          | TPtr Type
+          | TConst Type
+          | TArray [Maybe Int] Type
+          | TCustom String
+          deriving (Show)
