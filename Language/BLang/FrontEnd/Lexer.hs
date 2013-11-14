51a299c8cc99ab0adb5fac4c6ee215ae4600c015
suhorng <suhorng@gmail.com>
2013-11-14 15:31:33 +0800

    implement Lexer

--- Language/BLang/FrontEnd/Lexer.hs
+++ Language/BLang/FrontEnd/Lexer.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Language.BLang.FrontEnd.Lexer (
-  Token(..),
-  Literal(..),
-  lexer
+--  Token(..),
+--  Literal(..),
+--  lexer
 ) where
 
-import Language.BLang.FrontEnd.ParseMonad (Parser)
+import Text.Regex.Posix ((=~))
+import Data.Maybe (isJust)
+import Numeric (readDec, readFloat)
+
+import Language.BLang.FrontEnd.ParseMonad (Parser, getInput, advance)
 
 data Token = LiteralToken Literal
            | ID String
@@ -21,11 +27,66 @@ data Literal = IntLiteral Integer
              | StringLiteral String
              deriving (Show)
 
-symbols = [(["+","-","*","/"], SymArithmetic),
-           (["<=", ">=", "!=", "==", "<", ">"], SymRelational), -- note that the order is essential
-           (["||", "&&", "!"], SymLogic),
-           (["="], const SymAssign),
-           (["{", "}", "[", "]", "(", ")", ";", ",", "."], SymSeparator)]
+digit = "[0-9]"
+letter = "[A-Za-z]"
+
+symbols = [("(\\+|\\-|\\*|/)", SymArithmetic),
+           ("(<=|>=|!=|==|<|>)", SymRelational), -- note that the order is essential
+           ("(\\|\\||&&|!)", SymLogic),
+           ("=", const SymAssign),
+           ("({|}|\\[|\\]|\\(|\\)|;|,|\\.)", SymSeparator)]
+
+litFloat = concat ["((", digit, "*\\.", digit, "+|", digit, "+\\.)",
+                   "([eE][+-]?", digit, "+)?",
+                   "|(", digit, "+[eE][+=]?", digit, "+))"]
+litInt = digit ++ "+"
+identifier = concat ["(", letter, ")", "(", letter, "|", digit, "|_)*"]
+
+regExs :: [(String, String -> Token)]
+regExs = [(litFloat, LiteralToken . IntLiteral . fst . head . readDec),
+          (litInt, LiteralToken . FloatLiteral . fst . head . readFloat),
+          (identifier, ID)]
+       ++ symbols
+
+tryMatch :: String -> (String, String -> Token) -> Maybe (Int, Token)
+tryMatch haystack (needle, makeToken) =
+  if null before
+    then Just (length matched, makeToken matched)
+    else Nothing
+  where (before, matched, _, _) = haystack =~ needle :: (String, String, String, [String])
+
+litString :: String -> Parser String
+litString ('"':xs) = return . ('"':) =<< litString' xs
+  where litString' ('\\':c:xs) = return . ('\\':) . (c:) =<< litString' xs
+        litString' ('"':_) = return "\""
+        litString' (x:xs)  = return . (x:) =<< litString' xs
+        litString' _       = fail "Unterminated string"
+litString _ = fail "The input is not a string"
+
+comment :: String -> Parser Int
+comment ('/':'*':xs) = comment' 2 xs
+  where comment' !n ('*':'/':xs) = return (n + 2)
+        comment' !n (_:xs)       = comment' (n+1) xs
+        comment' _  _            = fail "Unterminated comment"
+comment _ = fail "The input is not a comment"
 
 lexer :: (Token -> Parser a) -> Parser a
-lexer = undefined
+lexer k = do
+  input <- getInput
+  case input of
+    [] ->
+      k EOF
+    '/':'*':xs -> do
+      len <- comment input
+      advance len
+      lexer k
+    c:xs | c `elem` " \f\t\v\r\n" ->
+      advance 1 >> lexer k
+    '"':xs -> do
+      str <- litString input
+      advance (length str)
+      k (LiteralToken (StringLiteral str))
+    otherwise ->
+      case filter isJust $ map (tryMatch input) regExs of
+        Just (len, tok):_ -> advance len >> k tok
+        [] -> fail "Input string does not match any token"
--- Main.hs
+++ Main.hs
@@ -3,7 +3,19 @@ module Main (main) where
 import qualified Language.BLang.FrontEnd.Lexer as Lexer
 import qualified Language.BLang.FrontEnd.Parser as Parser
 
+import qualified Language.BLang.FrontEnd.ParseMonad as ParseMonad -- for testing
+
+lexStream :: String -> Either ParseMonad.ParseError [Lexer.Token]
+lexStream = ParseMonad.runParser lexStream'
+  where lexStream' :: ParseMonad.Parser [Lexer.Token]
+        lexStream' = Lexer.lexer $ \token ->
+                      case token of
+                        Lexer.EOF -> return [Lexer.EOF]
+                        _         -> return . (token:) =<< lexStream'
+
 main :: IO ()
 main = do
-  let x = Parser.parse
-  return ()
+  input <- getContents
+  case lexStream input of
+    Left parseError -> putStrLn "[ERROR]" >> putStrLn (show parseError)
+    Right stream    -> print stream
