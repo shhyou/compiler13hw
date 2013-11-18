7fa029a6e14023a0421ce7bf0702052a14590db3
suhorng <suhorng@gmail.com>
2013-11-18 11:05:53 +0800

    add IOs

--- Main.hs
+++ Main.hs
@@ -1,29 +1,25 @@
 module Main (main) where
 
-import qualified Language.BLang.FrontEnd.Lexer as Lexer
+import System.IO (readFile)
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(..))
+
 import qualified Language.BLang.FrontEnd.Parser as Parser
-
 import qualified Language.BLang.Homework.Homework3 as Homework3
-
-import qualified Language.BLang.FrontEnd.ParseMonad as ParseMonad -- for testing
-import qualified Language.BLang.Debug.ParserAST as Debug.ParserAST (printAST)
-
-lexStream :: String -> Either ParseMonad.ParseError [Lexer.Token]
-lexStream = ParseMonad.runParser lexStream'
-  where lexStream' :: ParseMonad.Parser [Lexer.Token]
-        lexStream' = Lexer.lexer $ \token ->
-                      case token of
-                        Lexer.EOF -> return [Lexer.EOF]
-                        _         -> return . (token:) =<< lexStream'
 
 main :: IO ()
 main = do
-  input <- getContents
-{-
-  case lexStream input of
-    Left parseError -> putStrLn "[ERROR] [LEXER]" >> putStrLn (show parseError)
-    Right stream    -> putStrLn "[TOKEN]" >> mapM_ (\a -> putStr "  " >> print a) stream
--}
-  case Parser.parse input of
-    Left parseError -> putStrLn "[ERROR] [PARSER]" >> putStrLn (show parseError)
-    Right ast       -> Homework3.printAST ast
+  args <- getArgs
+  input <- case args of
+            [] -> getContents
+            [file] -> readFile file
+            _ -> putStrLn "[ERROR] Incorrect command line args" >> exitWith (ExitFailure 1)
+  let parseResult = Parser.parse input
+  case parseResult of
+    Left parseError -> do
+      putStrLn "[ERROR] [PARSER]"
+      putStrLn (show parseError)
+      exitWith (ExitFailure 1)
+    _ -> return ()
+  let Right ast = parseResult
+  Homework3.printAST ast
