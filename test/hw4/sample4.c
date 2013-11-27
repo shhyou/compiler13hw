accd53f845ffae97a5948725863faf156cecb1ef
LauZi <st61112@gmail.com>
2013-11-28 01:59:59 +0900

    hw4 testdata

---
+++ test/hw4/sample4.c
@@ -0,0 +1,35 @@
+float f;
+int g;
+int f() {
+    return g;
+}
+
+int h(int a) {
+    return a + 0.1;
+}
+int s(int a[]) {
+    typedef int I;
+    I f;
+    return f;
+}
+int main() {
+    int g;
+    int arr[1][2][3];
+    h(1, 2, 3);
+    h();
+    g = arr[1] + 3;
+    g = arr[1.1*3];
+    h(arr);
+    s(g);
+    return 0;
+}
+
+int k() {
+    int g;
+    float m;
+    p = 5;
+    return m;
+}
+void m() {
+    return 3;
+}
