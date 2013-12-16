180c115b9f933970aadf988aa54b78a8ac0df550
suhorng <suhorng@gmail.com>
2013-12-16 11:56:14 +0800

    add pair mapping function

--- Language/BLang/Miscellaneous.hs
+++ Language/BLang/Miscellaneous.hs
@@ -10,3 +10,10 @@ mapsnd f (a, t) = liftM2 (,) (return a) (f t)
 
 map2nd :: Monad m => (b -> m d) -> (a, b, c) -> m (a, d, c)
 map2nd f (a, t, c) = liftM3 (,,) (return a) (f t) (return c)
+
+first f (a, b) = (f a, b)
+second f (a, b) = (a, f b)
+
+first3 f (a, b, c) = (f a, b, c)
+second3 f (a, b, c) = (a, f b, c)
+third3 f (a, b, c) = (a, b, f c)
