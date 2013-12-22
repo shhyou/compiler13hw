a2934ba2b03fdfc774097a56ef8eb8171972ff3d
suhorng <suhorng@gmail.com>
2013-12-22 14:42:58 +0800

    update LLIR definition

--- Language/BLang/CodeGen/LLIR.hs
+++ Language/BLang/CodeGen/LLIR.hs
@@ -19,20 +19,30 @@ import qualified Language.BLang.Semantic.AST as S
         * For loop iteration shall be merged into `forCode`.
           Actually, I'm not sure whether we still need `For`.
 
-        * Assignment/variable reference is actually kind of strange now.
-
+        * Assignment/variable reference is actually kind of weird now.
             a = 5 + b;
-                Const (Reg 0 TInt) (IntLiteral 5)
-                Load (Reg 1 TInt) (Left "b")
-                Let (Reg 2 TInt) Plus (Reg 0 TInt) (Reg 1 TInt)
-                Store (Left "a") (Reg 2 TInt)
+                Load 0 (Left "b")
+                Let 1 Add (Constant (IntLiteral 5)) (Reg 0)
+                Store (Left "a") 1
 
-            p[0] = p[1] + 3.0
-                ??? something is missing
+            float p[100], q[2][3];
+            q[0][1] = p[1] + 3.0
+                ArrayRef 0 (Left "p") (Constant (IntLiteral 1)) 4
+                Load 1 (Right 0)
+                Let 2 Add (Reg 1) (Constant (FloatLiteral 3.0))
+                ArrayRef 3 (Left "q") (Constant (IntLiteral 0)) 12
+                ArrayRef 4 (Right 3) (Constant (IntLiteral 1)) 4
+                Store (Right 4) 2
+
+        * In short, `Var`/`Left name`/`ArrayRef` gives l-values. We need
+          `Load`/`Store` to access the saved value.
+
+          Standard ML has made references explicit, so is LLIR.
  -}
 
 data Prog v = Prog { progFuncs :: Assoc String (Func v)
-                   , progVars :: Assoc String v }
+                   , progVars :: Assoc String v
+                   , progRegs :: Assoc Int S.Type }
 
 data Func v = Func { funcName :: String
                    , funcVars :: Assoc String v -- **all** local variables, parameters and global variables
@@ -42,25 +52,25 @@ data Func v = Func { funcName :: String
 
 type Reg = Int
 
-data Value = LiteralVal Literal
+data Value = Constant Literal
+           | Var String                -- &string
            | Reg Reg
            deriving (Show)
 
-data AST = Phi Reg [Reg] -- register merging, can only appear in the very begin of every block
+data AST = Phi Reg [(String, Reg)] -- register merging, can only appear in the very begin of every block
          | Call Reg String [Value]
          | Let Reg Operator Value Value  -- let dst = src1 `op` src2
          | Load Reg (Either String Reg)  -- reg <- *(string|reg)
          | Store (Either String Reg) Reg -- *(string|reg) <- reg
-         | Var Reg String                -- reg <- &string
          | Cast { castDst :: Reg
                 , castDstType :: S.Type
                 , castSrc :: Reg
                 , castSrcType :: S.Type } -- dst :: t1 <- src :: t2
          | ArrayRef { refDst  :: Reg
-                    , refBase :: Reg
-                    , refIdx  :: Reg
+                    , refBase :: Either String Reg
+                    , refIdx  :: Value
                     , refSize :: Integer }
-         | Constant Reg Value -- constants
+         | Val Reg Value -- reg <- values
          | Branch Reg String String -- branch %res true-branch false-branch
          | Jump String
          | Return (Maybe Value)
--- Language/BLang/CodeGen/LLIRTrans.hs
+++ Language/BLang/CodeGen/LLIRTrans.hs
@@ -1,6 +1,7 @@
 -- module for transforming semantic IR into an ANF-inspired IR
 module Language.BLang.CodeGen.LLIRTrans where
 
+import qualified Language.BLang.Semantic.AST as S
 import qualified Language.BLang.CodeGen.LLIR as L
 
 
