6e0c4a322110462c9abf0d8adb98a8b6c13d1ae1
LauZi <st61112@gmail.com>
2013-12-29 04:37:44 +0900

    add prototypes for reg. alloc.

--- Language/BLang/CodeGen/MIPSTrans.hs
+++ Language/BLang/CodeGen/MIPSTrans.hs
@@ -11,6 +11,8 @@ import Language.BLang.Data
 data Obj = OVar String
          | OReg Int
          | OTxt String
+         | OInt
+         | OFloat
          deriving (Show)
 
 instance Ord Obj where
@@ -20,6 +22,8 @@ instance Ord Obj where
   (OVar _) <= _ = True
   (OReg _) <= _ = True
   (OTxt _) <= _ = True
+  (OInt _) <= _ = True
+  (OFloat _) <= _ = True
   _ <= _ = False
 
 data Addr = AReg A.Reg
@@ -29,26 +33,19 @@ data Addr = AReg A.Reg
           | AMadoka
           deriving (Show)
 
-type NameSpace = Assoc Obj Addr
+type NameSpace = Assoc Obj (S.Type, Addr)
+
+getOType (x, _) = case x of
+  S.TFloat -> OFloat
+  _ -> OInt
 
 
 newtype Foo a = Foo (a, [A.Inst], [A.DataVar], NameSpace) deriving (Show)
 
 runFoo :: Foo a -> (a, [A.Inst], [A.DataVar])
-runFoo (Foo (x, y, z)) = (x, reverse y, reverse z)
-
-runFoo' (Foo (_, y, z) = (reverse y, reverse z)
-
-rinst op args = Foo ((), [A.RType op args], [])
-iinst op rd rs imm = Foo ((), [A.IType op rd rs imm], [])
-jinst op imm = Foo ((), [A.JType op imm], [])
-label lbl = Foo ((), [A.Label lbl], [])
-
-linsts xs = Foo ((), xs, [])
-
-dtext txt = Foo ((), [], [A.Text txt])
-dword int = Foo ((), [], [A.Word [int]])
-dfloat dbl = Foo ((), [], [A.Float [dbl]])
+runFoo (Foo (x, y, z, _)) = (x, reverse y, reverse z)
+runFoo' (Foo (_, y, z, _) = (reverse y, reverse z)
+ns (Foo (_, _, _, ret)) = ret
 
 instance Functor (Foo a) where
   fmap f (Foo (x, y, z, w)) = Foo (f x, y, z, w)
@@ -59,6 +56,19 @@ instance Monad (Foo a) where
     let (Foo (x', y', z', w')) = f x
     in Foo (x', y' ++ y, z' ++ z, w' `unionA` w)
 
+rinst op args = Foo ((), [A.RType op args], [], emptyA)
+iinst op rd rs imm = Foo ((), [A.IType op rd rs imm], [], emptyA)
+jinst op imm = Foo ((), [A.JType op imm], [], emptyA)
+label lbl = Foo ((), [A.Label lbl], [], emptyA)
+
+linsts xs = Foo ((), xs, [], emptyA)
+
+dtext txt = Foo ((), [], [A.Text txt], emptyA)
+dword int = Foo ((), [], [A.Word [int]], emptyA)
+dfloat dbl = Foo ((), [], [A.Float [dbl]], emptyA)
+
+editNs ns = Foo ((), [], [], ns)
+editNs' = editNs . Assoc
 
 dataVars lblName = foldl folder []
   where folder xs (L.VarInfo vname vtype) = (lblName vname, A.Space (tySize vtype)):xs
@@ -73,20 +83,21 @@ transProg (L.Prog funcs globalVars regData) = A.Prog newData newFuncs newVars
     -- newFuncs :: [A.Func v]
     -- newVars :: Assoc String v    <- don't know what this is for
 
-    newData = dataVars ("GLOBAL_VAR_" ++) globalVars
+    globalVarLabel = ("GLOBAL_VAR_" ++)
+    newData = dataVars globalVarLabel globalVars
     newFuncs = undefined
-    newVars = undefined
+    newVars = map (\vname vtype -> (vtype, AData . globalVarLabel $ vname)) globalVars
 
     transFunc :: L.Func L.VarInfo -> A.Func v
     transFunc (L.Func fname fargs fvars fentry fcode) =
-      A.Func fname newFvars newFrameSize newFuncEnter newFuncCode newFuncData
+      A.Func fname newFuncVars newFrameSize newFuncEnter newFuncCode newFuncData
       where
         -- fname :: String
         -- fargs :: [(String, S.Type)]
         -- fvars :: Assoc String L.VarInfo
         -- fentry :: Int <- should be L.Label
         -- fcode :: Assoc L.Label [L.AST]
-        -- newFvars :: Assoc String v  <-- what?
+        -- newFuncVars :: Assoc String v  <-- what?
         -- newFrameSize :: Int
         -- newFuncEnter :: [A.Inst]
         -- newFuncCode :: [A.Inst]
@@ -95,9 +106,19 @@ transProg (L.Prog funcs globalVars regData) = A.Prog newData newFuncs newVars
         funcLabel = (fname ++ "_" ++)
         blockLabel = funcLabel . ("BLK_" ++)
         blockLabel' = blockLabel . show
+        localVarLabel = funcLabel . ("VAR_" ++)
+
+        newFuncVars = localVars `unionA` localArgs `unionA` globalVars
+          where
+            folder (acc, idx) (vname, vtype) = ((vtype, AMem idx A.FP):acc, idx + tySize vtype)
+            localArgs = Assoc . fst $ foldl folder ([], 0) fargs
+
+            folder' (acc, idx) (L.VarInfo vname vtype) = ((vtype, AMem idx' A.FP):acc, idx')
+              where idx' = idx - tySize vtype
+            localVars = Assoc . fst $ foldl folder' ([], 0) fvars
 
         newFrameSize = sum $ fmap (tySize . L.varType) fargs
-        newFuncData = dataVars (funcLabel . ("VAR_" ++)) fvars
+        newFuncData = dataVars localVarLabel fvars
 
         newFuncEnter = fst . runFoo' $ do
           iinst A.SW A.RA A.SP (Right -4)
@@ -107,7 +128,23 @@ transProg (L.Prog funcs globalVars regData) = A.Prog newData newFuncs newVars
           iinst A.SUB A.SP A.SP (Right (40 + newFrameSize))
           jinst A.J (Left . blockLabel $ show fentry)
 
-        -- newReg, findReg, freeReg, showLiteral, getVal
+        -- alloc, load, loadTo, finale
+        spill x ns = undefined
+
+        alloc OInt ns = undefined
+        alloc OFloat ns = undefined
+        alloc x ns = alloc . getOType $ ns ! x
+
+        load (OVar var) ns = undefined
+        load (OReg reg) ns = undefined
+        load (OTxt lbl) ns = undefined
+
+        loadTo (OVar var) ns = undefined
+        loadTo (OReg reg) ns = undefined
+        loadTo (OTxt lbl) ns = undefined
+
+        finale x ns = editNs' [(x, (fst $ ns ! x, TMadoka))]
+
 
         transBlock :: [L.AST] -> ([A.Inst], [A.DataVar])
         transBlock = runFoo' . foldl transInst (return ())
@@ -121,13 +158,13 @@ transProg (L.Prog funcs globalVars regData) = A.Prog newData newFuncs newVars
                 (L.Call rd "read" []) -> do
                   iinst A.LW (A.VReg 0) A.ZERO (Right 5)
                   rinst A.SYSCALL []
-                  rd' <- alloc rd
+                  rd' <- ns>>=alloc rd
                   rinst A.ADD [rd', A.VReg 0, A.ZERO]
 
                 (L.Call rd "fread" []) -> do
                   iinst A.LW (A.VReg 0) A.ZERO (Right 5)
                   rinst A.SYSCALL []
-                  rd' <- alloc rd
+                  rd' <- ns>>=alloc rd
                   rinst A.MOVES [rd', A.FReg 0]
 
                 (L.Call rd fname args) -> undefined
