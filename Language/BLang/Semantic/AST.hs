fb1acb374a838eba17d762d7e92788592dce1231
suhorng <suhorng@gmail.com>
2014-01-02 03:44:00 +0800

    print AST

--- Language/BLang/Semantic/AST.hs
+++ Language/BLang/Semantic/AST.hs
@@ -43,7 +43,62 @@ data AST v = Block (Assoc String v) [AST v] -- retain block structure and declar
            | LiteralVal Literal
            | ArrayRef Type (AST v) (AST v) -- ArrayRef (Identifier "a") (LiteralVal (IntLiteral 0))
            | Nop
-           deriving (Show)
+
+instance Show v => Show (AST v) where
+  show (Block tbl asts) =
+    "{\n  " ++ show tbl ++ "\n"
+    ++ concatMap (("  " ++) . (++ "\n")) (concatMap (split '\n' . show) asts)
+    ++ "}"
+  show (Expr ty rator rands) =
+    "(" ++ ratorText ++ " " ++ intercalate " " (map show rands) ++ ")"
+    where Just ratorText = lookup rator ratorTable
+          ratorTable = [(Plus, "+"), (Minus, "-"), (Times, "*"), (Divide, "/"), (Negate, "-"),
+                        (LEQ, "<="), (GEQ, ">="), (LOr, "||"), (LAnd, "&&"), (LNot, "!"), (Assign, ":="),
+                        (Language.BLang.FrontEnd.Parser.LT, "<"),
+                        (Language.BLang.FrontEnd.Parser.GT, ">"),
+                        (Language.BLang.FrontEnd.Parser.EQ, "=="), (NEQ, "/=")]
+    -- "(" ++ show rator ++ ":" ++ showsPrec 11 ty [] ++ " "
+    -- ++ intercalate " " (map show rands) ++ ")"
+  show (ImplicitCast ty' ty e) =
+    "(" ++ showsPrec 11 ty [] ++ "->" ++ showsPrec 11 ty' []
+    ++ " " ++ show e ++ ")"
+  show (For forinit forcond foriter forcode) =
+    "for (" ++ intercalate "," (map show forinit)
+    ++ "; " ++ intercalate "," (map show forcond)
+    ++ "; " ++ intercalate "," (map show foriter)
+    ++ ")\n" ++ showBlocked forcode
+  show (While whcond whcode) =
+    "while (" ++ show whcond ++ ")\n" ++ showBlocked whcode
+  show (Ap ty fn args) =
+    "(" ++ show fn ++ concatMap ((' ':) . show) args ++ ")"
+  show (If con th el) =
+    "if (" ++ show con ++ ")\n"
+    ++ showBlocked th
+    ++ case el of { Just el' -> "\nelse\n" ++ showBlocked el'; Nothing -> "" }
+  show (Return val) =
+    "return(" ++ case val of { Just val' -> show val'; Nothing -> "" } ++ ")"
+  show (Identifier ty name) =
+    name
+  show (LiteralVal (IntLiteral n)) =
+    show n
+  show (LiteralVal (FloatLiteral f)) =
+    show f
+  show (LiteralVal (StringLiteral s)) =
+    show s
+  show (ArrayRef ty ref idx) =
+    show ref ++ "[" ++ show idx ++ "]"
+  show Nop =
+    "()"
+
+showBlocked c@(Block _ _) = show c
+showBlocked c             = "  " ++ intercalate "\n  " (split '\n' $ show c) ++ ";"
+
+split :: Eq a => a -> [a] -> [[a]]
+split c []    = [[]]
+split c (c':rest)
+  | c == c'   = []:hd:tl
+  | otherwise = (c':hd):tl
+  where hd:tl = split c rest
 
 getType :: AST v -> Type
 getType (Block _ _) = TVoid
